247042518e6444cc40600abd29f28371c684655f
Jason Rummel <sunward6@gmail.com>
2017-04-26 17:11:34 -0500

    Create SwipeView.java

---
+++ app/src/main/java/seromatch/seromatchtest/SwipeView.java
@@ -0,0 +1,54 @@
+package seromatch.seromatchtest;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+
+public class SwipeView extends AppCompatActivity
+{
+    private SwipePlaceHolderView mSwipeView;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.swipetab);
+
+        mSwipeView = (SwipePlaceHolderView)findViewById(R.id.swipeView);
+        mContext = getApplicationContext();
+
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(3)
+                .setSwipeDecor(new SwipeDecor()
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.swipe_in_msg)
+                        .setSwipeOutMsgLayoutId(R.layout.swipe_out_msg));
+
+
+        for(Profile profile : Utils.loadProfiles(this.getApplicationContext())){
+            mSwipeView.addView(new MatchCard(mContext, profile, mSwipeView));
+        }
+
+        findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(false);
+            }
+        });
+
+        findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(true);
+            }
+        });
+    }
+}
